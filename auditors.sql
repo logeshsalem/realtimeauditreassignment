41cdf98dff9a2f3b5d721a0ace1b703ca18dec93
Logeshwaran <141913551+logeshsalem@users.noreply.github.com>
2025-11-17 16:24:14 +0530

    Update auditors.sql

--- auditors.sql
+++ auditors.sql
@@ -4,8 +4,8 @@ create table auditors(
 	home_lat double precision,
 	home_lon double precision,
 	availability_status varchar(20) not null,
-	workload_capacity_hours integer,
-	current_assigned_hours integer
+	workload_capacity_hours double precision,
+	current_assigned_hours double precision
 );
 
 select * from auditors;
@@ -13,7 +13,7 @@ select * from auditors;
 drop table auditors;
 
 create table store(
-id bigserial primary key,
+store_id bigserial primary key,
 name varchar(255) not null,
 address text,
 location_lat double precision not null,
@@ -23,6 +23,16 @@ store_status varchar(20) not null
 
 select * from store;
 
+drop table store;
+
+CREATE TABLE audit_plan (
+    audit_id BIGSERIAL PRIMARY KEY,    
+    auditor_id BIGINT REFERENCES auditors(auditor_id) ON DELETE SET NULL,
+    store_id BIGINT REFERENCES store(store_id) ON DELETE CASCADE,   
+    audit_priority VARCHAR(20) NOT NULL, -- High / Medium / Low  
+    audit_status VARCHAR(20) NOT NULL 
+);
+
 select * from audit_plan;
 
-
+drop table audit_plan;
